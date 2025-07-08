777dedd27f3b8d86047ed7ebc6d25fd1779a46f7
MStud01 <mnouma01@student.ubc.ca>
2025-07-08 23:34:40 +0300

    Add JUnit 5 package and tests for DeterminedString class

---
+++ src/test/model/TestDeterminedString.java
@@ -0,0 +1,80 @@
+package test.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.DeterminedString;
+
+
+public class TestDeterminedString {
+    private DeterminedString testDS1;
+    private DeterminedString testDS2;
+    private DeterminedString testDS3;
+
+    @BeforeEach
+    void runBefore() {
+        testDS1 = new DeterminedString();
+        testDS2 = new DeterminedString("abc", false);
+        testDS3 = new DeterminedString("test", true);
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("", testDS1.getString());
+        assertTrue(testDS1.isNotWord());
+        assertFalse(testDS1.isWord());
+
+        assertEquals("abc", testDS2.getString());
+        assertTrue(testDS2.isNotWord());
+        assertFalse(testDS2.isWord());
+
+        assertEquals("test", testDS3.getString());
+        assertTrue(testDS3.isWord());
+        assertFalse(testDS3.isNotWord());
+    }
+
+    @Test
+    void setStatusUnchangedFalse() {
+        assertTrue(testDS1.isNotWord());
+        assertFalse(testDS1.isWord());
+
+        testDS1.setStatus(false);
+        
+        assertTrue(testDS1.isNotWord());
+        assertFalse(testDS1.isWord());
+    }
+
+    @Test
+    void setStatusUnchangedTrue() {
+        assertTrue(testDS3.isWord());
+        assertFalse(testDS3.isNotWord());
+
+        testDS3.setStatus(true);
+        
+        assertTrue(testDS3.isWord());
+        assertFalse(testDS3.isNotWord());
+    }
+
+    @Test
+    void setStatusChangedtoTrue() {
+        assertTrue(testDS2.isNotWord());
+        assertFalse(testDS2.isWord());
+
+        testDS2.setStatus(true);
+        
+        assertTrue(testDS2.isWord());
+        assertFalse(testDS2.isNotWord());
+    }
+
+    @Test
+    void setStatusChangedtoFalse() {
+        assertTrue(testDS3.isWord());
+        assertFalse(testDS3.isNotWord());
+
+        testDS3.setStatus(false);
+        
+        assertTrue(testDS3.isNotWord());
+        assertFalse(testDS3.isWord());
+    }
+}
